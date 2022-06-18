63401be7f8ba5ecea84156202b21944982da6446
Isabel Maccabee <isabel.maccabee@gmail.com>
2022-06-18 20:39:56 +0100

    add test to check multiple airports are handled

--- src/__tests__/HolidaySearch.test.ts
+++ src/__tests__/HolidaySearch.test.ts
@@ -8,14 +8,14 @@ describe('HolidaySearch class', () => {
     departureDate: '2023-07-01',
     duration: 7,
   });
-  test('should have InputQuery property that retrives search query', () => {
+  test('has InputQuery property that retrives search query', () => {
     expect(testHolidaySearch1.InputQuery).toBe('Departing from: MAN, Travelling to: AGP, Departing date: 2023-07-01, Duration: 7 day(s)');
   });
-  test('should have FirstResult property that returns first valid flight/hotel combo', () => {
+  test('has FirstResult property that returns first valid flight/hotel combo', () => {
     expect(testHolidaySearch1.FirstResult.Flight.Id).toEqual(2);
     expect(testHolidaySearch1.FirstResult.Hotel.Id).toEqual(9);
   });
-  test('should have Results property that returns list of valid flight/hotel combos with simple single result', () => {
+  test('has Results property that returns list of valid flight/hotel combos with simple single result', () => {
     const validFlight = {
       id: 2,
       airline: 'Oceanic Airlines',
@@ -39,7 +39,7 @@ describe('HolidaySearch class', () => {
     expect(testHolidaySearch1.Results).toHaveLength(1);
     expect(testHolidaySearch1.Results).toEqual([validResult]);
   });
-  test('should have Results property that combines all valid flight and hotels into combos', () => {
+  test('has Results property that combines all valid flight and hotels into combos', () => {
     const testHolidaySearch2 = new HolidaySearch({
       departingFrom: ['LGW'],
       travellingTo: 'AGP',
@@ -82,7 +82,7 @@ describe('HolidaySearch class', () => {
     expect(testHolidaySearch2.Results).toHaveLength(2);
     expect(testHolidaySearch2.Results).toEqual(expect.arrayContaining(validResults));
   });
-  test('should have Results property that orders the flight/hotel combos by price (low to high)', () => {
+  test('has Results property that orders the flight/hotel combos by price (low to high)', () => {
     const testHolidaySearch2 = new HolidaySearch({
       departingFrom: ['LGW'],
       travellingTo: 'AGP',
@@ -94,5 +94,57 @@ describe('HolidaySearch class', () => {
     const result2 = testHolidaySearch2.Results[1];
 
     expect(result1.TotalPrice).toBeLessThan(result2.TotalPrice);
+  });
+  test('handles multiple departure airport options', () => {
+    const testHolidaySearch3 = new HolidaySearch({
+      departingFrom: ['LTN', 'LGW'],
+      travellingTo: 'PMI',
+      departureDate: '2023-06-15',
+      duration: 10,
+    });
+
+    const validFlight1 = {
+      id: 4,
+      airline: 'Trans American Airlines',
+      from: 'LTN',
+      to: 'PMI',
+      price: 153,
+      departure_date: '2023-06-15',
+
+    };
+    const validFlight2 = {
+      id: 6,
+      airline: 'Fresh Airways',
+      from: 'LGW',
+      to: 'PMI',
+      price: 75,
+      departure_date: '2023-06-15',
+    };
+
+    const validHotel1 = {
+      id: 5,
+      name: 'Sol Katmandu Park & Resort',
+      arrival_date: '2023-06-15',
+      price_per_night: 60,
+      local_airports: ['PMI'],
+      nights: 10,
+    };
+    const validHotel2 = {
+      id: 13,
+      name: 'Jumeirah Port Soller',
+      arrival_date: '2023-06-15',
+      price_per_night: 295,
+      local_airports: ['PMI'],
+      nights: 10,
+    };
+
+    const result1 = new Result(validFlight1, validHotel1);
+    const result2 = new Result(validFlight1, validHotel2);
+    const result3 = new Result(validFlight2, validHotel1);
+    const result4 = new Result(validFlight2, validHotel2);
+
+    expect(testHolidaySearch3.Results).toEqual(
+      expect.arrayContaining([result1, result2, result3, result4]),
+    );
   });
 });
